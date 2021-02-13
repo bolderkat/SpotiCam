d23fee854678023e97f837a98748c0ae905d7fb5
Daniel Luo <d.luo117@gmail.com>
2021-02-12 16:13:57 -0800

    Change to getting dominant color from center of image

--- SpotiCam/Views/Processing/SCProcessingViewController.m
+++ SpotiCam/Views/Processing/SCProcessingViewController.m
@@ -12,8 +12,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *danceabilityLabel;
 @property (weak, nonatomic) IBOutlet UILabel *energyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *valenceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *hLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bLabel;
 @property (weak, nonatomic) IBOutlet UIView *colorView;
-@property (nonatomic) UIColor *averageColor;
+@property (nonatomic) UIColor *dominantColor;
 
 @end
 
@@ -22,9 +25,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.imageView.image = self.image;
-    self.averageColor = [self getAverageColorFromPhoto];
-    self.colorView.backgroundColor = self.averageColor;
-    [self calculateTrackAttributesFromColor:self.averageColor];
+    self.dominantColor = [self getDominantColorFromPhoto];
+    self.colorView.backgroundColor = self.dominantColor;
+    [self calculateTrackAttributesFromColor:self.dominantColor];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -32,10 +35,14 @@
     [self.navigationController setNavigationBarHidden:NO];
 }
 
-- (UIColor*)getAverageColorFromPhoto {
-    // Translated to Obj-C from https://www.hackingwithswift.com/example-code/media/how-to-read-the-average-color-of-a-uiimage-using-ciareaaverage
+- (UIColor*)getDominantColorFromPhoto {
+    // Adapted from https://www.hackingwithswift.com/example-code/media/how-to-read-the-average-color-of-a-uiimage-using-ciareaaverage
     CIImage *image = [CIImage imageWithCGImage:self.image.CGImage];
-    CIVector *extentVector = [CIVector vectorWithX:image.extent.origin.x Y:image.extent.origin.y Z:image.extent.size.width W:image.extent.size.height];
+    CGFloat width = image.extent.size.width;
+    CGFloat height = image.extent.size.height;
+    
+    // Use central area of photo only for averaging
+    CIVector *extentVector = [CIVector vectorWithX:image.extent.origin.x + width / 4 Y:image.extent.origin.y + height / 4 Z:width / 2 W:height / 2];
     CIFilter *filter = [CIFilter filterWithName:@"CIAreaAverage" withInputParameters:@{kCIInputImageKey: image, kCIInputExtentKey: extentVector}];
     CIImage *outputImage = [filter outputImage];
     
@@ -43,13 +50,27 @@
     CIContext *context = [CIContext contextWithOptions:@{kCIContextWorkingColorSpace: [NSNull null]}];
     [context render:outputImage toBitmap:bitmap rowBytes:4 bounds:CGRectMake(0, 0, 1, 1) format:kCIFormatRGBA8 colorSpace:nil];
     
-    return [UIColor colorWithRed:(CGFloat)bitmap[0] / 255 green:(CGFloat)bitmap[1] / 255 blue:(CGFloat)bitmap[2] / 255 alpha:(CGFloat)bitmap[3] / 255];
+    UIColor *averageColor = [UIColor colorWithRed:(CGFloat)bitmap[0] / 255 green:(CGFloat)bitmap[1] / 255 blue:(CGFloat)bitmap[2] / 255 alpha:(CGFloat)bitmap[3] / 255];
+    
+    // Increase saturation of color to get dominant color from average
+    CGFloat h, s, b, a;
+    [averageColor getHue:&h saturation:&s brightness:&b alpha:&a];
+    s += 0.25;
+    
+    return [UIColor colorWithHue:h saturation:s brightness:b alpha:a];
+    
 }
 
 -(void)calculateTrackAttributesFromColor:(UIColor*)color {
     SCAPIManager *apiManager = [[SCAPIManager alloc]initWithColor:color];
+    
+    CGFloat h, s, b, a;
+    [self.dominantColor getHue:&h saturation:&s brightness:&b alpha:&a];
     self.danceabilityLabel.text = [NSString stringWithFormat:@"Danceability: %.2f", apiManager.danceability];
     self.energyLabel.text = [NSString stringWithFormat:@"Energy: %.2f", apiManager.energy];
     self.valenceLabel.text = [NSString stringWithFormat:@"Valence: %.2f", apiManager.valence];
+    self.hLabel.text = [NSString stringWithFormat:@"H: %.2f", h];
+    self.sLabel.text = [NSString stringWithFormat:@"S: %.2f", s];
+    self.bLabel.text = [NSString stringWithFormat:@"B: %.2f", b];
 }
 @end
