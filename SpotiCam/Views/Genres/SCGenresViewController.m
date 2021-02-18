df08909255e6538334dec3185a470906bd8b3c74
Daniel Luo <d.luo117@gmail.com>
2021-02-17 20:05:29 -0800

    Display fetched genres on tableView using diffable data source

--- SpotiCam/Views/Genres/SCGenresViewController.m
+++ SpotiCam/Views/Genres/SCGenresViewController.m
@@ -10,8 +10,10 @@
 #import <OIDAuthState.h>
 
 @interface SCGenresViewController ()
+@property (nonatomic) NSArray<NSString*> *genres;
 @property (weak, nonatomic) IBOutlet UITableView *genreTable;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property UITableViewDiffableDataSource *dataSource;
 
 @end
 
@@ -20,14 +22,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"Select Genres";
+    [self configureDataSource];
     [self configureTableView];
     [self configureSearchBar];
     [self fetchGenres];
 }
 
-- (void)configureTableView {
-    self.genreTable.delegate = self;
-}
+
+
 
 - (void)configureSearchBar {
     self.searchBar.delegate = self;
@@ -40,16 +42,42 @@
         }
         
         [SCAPIManager fetchGenreSeedsWithToken:accessToken completion:^(NSArray<NSString *> *array) {
-            [self displayGenresWithArray:array];
+            NSMutableArray *capitalizedArray = [NSMutableArray array];
+            for (NSString *item in array) {
+                [capitalizedArray addObject:[(NSString*)item capitalizedString]];
+            }
+            self.genres = [NSArray arrayWithArray:capitalizedArray];
+            [self updateTableView];
         }];
     }];
 }
 
-- (void)displayGenresWithArray:(NSArray<NSString*>*)array {
-    for (NSString *item in array) {
-        NSLog(@"%@\n", item);
-    }
+#pragma mark - Table View Diffable Data Source and Delegate
+- (void)configureDataSource {
+    self.dataSource = [[UITableViewDiffableDataSource alloc] initWithTableView:self.genreTable cellProvider:^UITableViewCell * _Nullable(UITableView *tableView, NSIndexPath *indexPath, id title) {
+        UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"genreCell"];
+        cell.textLabel.text = [self.genres objectAtIndex:indexPath.row];
+        return cell;
+    }];
 }
+
+-(void)updateTableView {
+    NSDiffableDataSourceSnapshot *snapshot = [NSDiffableDataSourceSnapshot new];
+    [snapshot appendSectionsWithIdentifiers:@[@"Main"]];
+    [snapshot appendItemsWithIdentifiers:self.genres];
+    [self.dataSource applySnapshot:snapshot animatingDifferences:YES];
+    
+}
+
+- (void)configureTableView {
+    self.genreTable.delegate = self;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
 
 
 @end
